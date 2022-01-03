e5a4250a52e4d4075a445e1c6199c72e15523bea
Attila Soltesz <attila.soltesz13@gmail.com>
2022-01-03 23:58:05 +0100

    Add spendings model

--- app/spendingstracker/models.py
+++ app/spendingstracker/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Spending(models.Model):
+    """
+    Spending object.
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    spended_value = models.DecimalField(max_digits=10, decimal_places=2)
+    currency = models.CharField(max_length=255, blank=False)
+    description = models.CharField(max_length=255, blank=True)
